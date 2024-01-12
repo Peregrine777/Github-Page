de82e3ae7733aec40840c428217ce755910d139e
Samuel McKenzie-Sell <samuel.m.mckenzie-sell@student.uts.edu.au>
2024-01-12 16:05:02 +1100

    Change to single webpage

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -20,7 +20,7 @@ module.exports = {
             ]
         }),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/threejs.html'),
+            template: path.resolve(__dirname, '../src/index.html'),
             minify: true
         }),
         new MiniCSSExtractPlugin()
