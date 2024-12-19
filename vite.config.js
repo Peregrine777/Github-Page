ae20c90d594ee66a04c0c5d0d28d56f8fe550959
Samuel McKenzie-Sell <samuel.m.mckenzie-sell@student.uts.edu.au>
2024-12-19 18:55:46 +1100

    Edit update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Reclaimer/', // The repo name is 'Reclaimer', update this if your repo name is different.
+  base: '/Github-Page/', // The repo name is 'Reclaimer', update this if your repo name is different.
   build: {
     outDir: 'dist',
   },
