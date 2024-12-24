2a316433219d29a8a8bf9bbddf637a0e4ab7348f
Peregrine777 <Peregrine777@users.noreply.github.com>
2024-12-24 05:28:16 +0000

    deploy: ef009fedbbc09eb6dddcb12052ee1f48ac8747f4

--- assets/TerrainBuilder_threaded_worker-CE_mdduR.js
+++ assets/TerrainBuilder_threaded_worker-CE_mdduR.js
@@ -22,7 +22,7 @@ self.onmessage = async function (e) {
     
     //yield until FBM is loaded
     while (!FBM) {
-      await new Promise((resolve) => setTimeout(resolve, 100));
+      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
     
 
