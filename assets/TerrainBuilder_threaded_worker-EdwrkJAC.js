65edb1394b088ed77244370b825ba325c5013617
Peregrine777 <Peregrine777@users.noreply.github.com>
2025-01-02 03:56:11 +0000

    deploy: edecc39251c217054879c5a04f35faadde990cef

--- assets/TerrainBuilder_threaded_worker-EdwrkJAC.js
+++ assets/TerrainBuilder_threaded_worker-EdwrkJAC.js
@@ -1,5 +1,5 @@
-import { ImprovedNoise } from 'three/addons/math/ImprovedNoise.js';
-// import { FBM } from '../Utils/FBM.js';
+import { ImprovedNoise } from '../Utils/ImprovedNoise.js';
+import { FBM } from '../Utils/FBM.js';
 
 console.log('Worker loaded successfully');
 
@@ -32,7 +32,7 @@ self.onmessage = function (e) {
 
 
 function buildChunk(center, size, planeSize, resolution, noiseParams) {
-  const n = new ImprovedNoise();
+  //const n = new ImprovedNoise();
 
 
   // let heightmap = FBM(center,
