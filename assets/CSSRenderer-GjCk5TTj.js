0bb10127a593924452ce291e352cd4bbfb6de7f9
Peregrine777 <Peregrine777@users.noreply.github.com>
2025-03-03 04:13:29 +0000

    deploy: 6a11825810829bf0c1c9d3a3530e34e59c94bd20

---
+++ assets/CSSRenderer-GjCk5TTj.js
@@ -0,0 +1,2 @@
+import{S as r}from"./index-cL6PTX_O.js";class n extends r{constructor(e){super(e),this.scene.name="CSSRenderer"}update(e){super.update(e)}}export{n as sc_CSSRenderer};
+//# sourceMappingURL=CSSRenderer-GjCk5TTj.js.map
