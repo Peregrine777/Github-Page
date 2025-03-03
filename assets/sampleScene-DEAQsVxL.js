766acfca1ee6be7c4d8368762f4d595f7fd21963
Peregrine777 <Peregrine777@users.noreply.github.com>
2025-03-03 04:55:52 +0000

    deploy: 0b74b6c6739de757080e622ac28154e39d1f2d23

--- assets/sampleScene-DEAQsVxL.js
+++
@@ -1,2 +0,0 @@
-import{S as l,B as u,M as d,b as h,C as s,ao as m,ap as p}from"./index-cL6PTX_O.js";class g extends l{constructor(e){super(e);const t=new u,a=new d({color:65280}),n=new h(t,a);this.scene.add(n);let o=new s(.5,.72,1);const c=new m(o,.5);this.scene.add(c);let r=new s(1,.98,.8);const i=new p(r,1);this.scene.add(i)}update(e){super.update(e)}}export{g as sc_SampleScene};
-//# sourceMappingURL=sampleScene-DEAQsVxL.js.map
