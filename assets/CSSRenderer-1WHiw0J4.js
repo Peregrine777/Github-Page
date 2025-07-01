50ba5b7f11a85db87027259ab8c54b6ce8323c38
Peregrine777 <Peregrine777@users.noreply.github.com>
2025-07-01 05:09:29 +0000

    deploy: f904ec83857d2702560f673b43ddc68e415a426f

---
+++ assets/CSSRenderer-1WHiw0J4.js
@@ -0,0 +1,2 @@
+import{S as r}from"./index-aLiawv2U.js";class n extends r{constructor(e){super(e),this.scene.name="CSSRenderer"}update(e){super.update(e)}}export{n as sc_CSSRenderer};
+//# sourceMappingURL=CSSRenderer-1WHiw0J4.js.map
