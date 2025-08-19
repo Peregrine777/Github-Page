b413f67dcc4bf808680835b50fe3d14536400034
Peregrine777 <Peregrine777@users.noreply.github.com>
2025-08-19 04:59:28 +0000

    deploy: b20d54a0c3b386d42cd9a862f0210635b55f7d75

--- assets/CSSRenderer-CRHFvBxu.js
+++
@@ -1,2 +0,0 @@
-import{S as r}from"./index-D8NRlxVE.js";class n extends r{constructor(e){super(e),this.scene.name="CSSRenderer"}update(e){super.update(e)}}export{n as sc_CSSRenderer};
-//# sourceMappingURL=CSSRenderer-CRHFvBxu.js.map
