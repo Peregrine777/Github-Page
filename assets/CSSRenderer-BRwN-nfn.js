4887c5f4276e3dcf725d8eb87154b81d300a9b55
Peregrine777 <Peregrine777@users.noreply.github.com>
2025-08-19 05:20:15 +0000

    deploy: d613a49d418e138cde24f3d5d5b932e4bddc4d5a

--- assets/CSSRenderer-BRwN-nfn.js
+++
@@ -1,2 +0,0 @@
-import{S as r}from"./index-DO8cTj49.js";class n extends r{constructor(e){super(e),this.scene.name="CSSRenderer"}update(e){super.update(e)}}export{n as sc_CSSRenderer};
-//# sourceMappingURL=CSSRenderer-BRwN-nfn.js.map
